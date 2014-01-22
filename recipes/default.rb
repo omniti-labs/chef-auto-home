202e772d146be2434f065207476feb0e8a56361b
Mark Harrison <mark@omniti.com>
2014-01-22 14:21:30 -0500

    Whitespace fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,17 +2,17 @@
 return unless node[:auto_home][:enabled]
 
 template "/etc/auto_home" do
-	source "auto_home.erb"
-	owner "root"
-	group "bin"
-	mode 0751
-        variables(
-            :base => node[:auto_home][:base]
-        )
-	notifies :restart, "service[autofs]", :immediately 
+    source "auto_home.erb"
+    owner "root"
+    group "bin"
+    mode 0751
+    variables(
+        :base => node[:auto_home][:base]
+    )
+    notifies :restart, "service[autofs]", :immediately 
 end
 
 service "autofs" do
-	supports :restart => true, :status => true
-	action :nothing
+    supports :restart => true, :status => true
+    action :nothing
 end
